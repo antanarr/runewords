eb5a5499c85df0bb42b07ee020ddc50323710503
Anthony Yarand <alyarand@icloud.com>
2025-07-18 05:27:49 -0500

    Initial Commit

---
+++ RuneWords/RuneWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuneWordsApp.swift
+//  RuneWords
+//
+//  Created by Anthony Yarand on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RuneWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RuneWordsTests/RuneWordsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RuneWordsTests.swift
+//  RuneWordsTests
+//
+//  Created by Anthony Yarand on 7/18/25.
+//
+
+import Testing
+@testable import RuneWords
+
+struct RuneWordsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
